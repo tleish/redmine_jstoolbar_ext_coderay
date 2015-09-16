27d69329f007840ae4ea9ba8bedaeb323d64c1c2
Thomas Leishman <thomas.leishman@plansource.com>
2015-09-16 14:04:42 -0600

    adding support for Redmine 3

--- init.rb
+++ init.rb
@@ -5,7 +5,7 @@ Redmine::Plugin.register :redmine_jstoolbar_ext_coderay do
   name 'Redmine jsToolbar Coderay Extension'
   author 'Thomas Leishman'
   description 'The Redmine JS Toolbar Coderay Extension adds a CodeRay button and submenu to the jsToolbar.'
-  version '0.1.0'
+  version '0.2.0'
   url 'https://github.com/tleish'
   author_url 'https://github.com/tleish'
   requires_redmine_plugin :redmine_jstoolbar_ext, :version_or_higher => '0.1.0'
